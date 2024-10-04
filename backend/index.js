9100818d6a5ad78e05351d903bc2b4009f12a69e
juanjk24 <juanjkuellar24@gmail.com>
2024-10-04 18:53:28 -0500

    Add user routes

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,14 @@ import express from "express";
 import cors from "cors";
 
 import { PORT } from "./config/global.js";
+import { usersRouter } from "./routes/users.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.disable("x-powered-by");
 
-app.get("/users", (req, res) => {
-  res.send("ruta para obtener todos los usuarios");
-});
-
-app.post("/users", (req, res) => {
-  res.send("ruta para crear un usario (siendo admin)");
-});
-
-app.put("/users", (req, res) => {
-  res.send("ruta para actualizar un usuario");
-});
-
-app.delete("/users", (req, res) => {
-  res.send("ruta para eliminar un usuario (siendo admin)");
-});
+app.use('/users', usersRouter)
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
---
+++ backend/routes/users.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+import { UserController } from "../controllers/users.js";
+
+export const usersRouter = Router();
+
+usersRouter.get("/", UserController.getAll);
+usersRouter.get("/:id", UserController.getById);
+
+usersRouter.post("/", UserController.create);
+usersRouter.put("/:id", UserController.update);
+usersRouter.delete("/:id", UserController.delete);
