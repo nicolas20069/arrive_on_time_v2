9d2adeae4492f5ce3f2c8391405cf64eff88039e
juanjk24 <juanjkuellar24@gmail.com>
2024-10-05 00:35:34 -0500

    Add routing and favicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeicons/primeicons.css";
+
+import { Employees } from "./admin/modules/employees/index.jsx";
 
 function App() {
   return (
-    <>
-      <h1>Arrive on time</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Iniciar Sesion</div>} />
+        <Route path="/admin" element={<Employees/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
