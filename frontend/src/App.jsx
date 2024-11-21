f3feb8c51c7acee9615b7e902f124620591ac0fb
juanjk24 <juanjkuellar24@gmail.com>
2024-11-21 15:54:58 -0500

    Add routes for admin modules

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,19 @@ import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primeicons/primeicons.css";
 
 import { Employees } from "./admin/modules/employees/index.jsx";
+import { Dashboard } from "./admin/modules/dashboard/index.jsx";
+import { Reports } from "./admin/modules/reports/index.jsx";
+import { Attendance } from "./admin/modules/attendances/index.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div>Iniciar Sesion</div>} />
-        <Route path="/admin" element={<Employees/>} />
+        <Route path="/admin" element={<Dashboard/>} />
+        <Route path="/admin/employees" element={<Employees/>} />
+        <Route path="/admin/reports" element={<Reports/>} />
+        <Route path="/admin/attendances" element={<Attendance/>} />
       </Routes>
     </BrowserRouter>
   )
