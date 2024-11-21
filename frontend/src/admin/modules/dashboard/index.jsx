ffeb28cbac1841b4e9302f3021aecc92de3c9948
juanjk24 <juanjkuellar24@gmail.com>
2024-11-21 16:51:28 -0500

    Add dashboard module

--- frontend/src/admin/modules/dashboard/index.jsx
+++ frontend/src/admin/modules/dashboard/index.jsx
@@ -1,10 +1,50 @@
 import { Header } from "../../includes/Header.jsx";
+import "./styles/main.css";
 
 export function Dashboard() {
   return (
     <>
       <Header />
-      <h1>Dashboard</h1>
+
+      <section className="dashboard">
+        <aside className="dashboard-aside">
+          <h1>Dashboard</h1>
+        </aside>
+
+        <main className="dashboard-main">
+          <article>
+            <img src="/dashboard/users.svg" alt="" />
+            <div>
+              <h2>Gestionar usuarios</h2>
+              <span>Haz click aqui</span>
+            </div>
+          </article>
+
+          <article>
+            <img src="/dashboard/users.svg" alt="" />
+            <div>
+              <h2>Gestionar usuarios</h2>
+              <span>Haz click aqui</span>
+            </div>
+          </article>
+
+          <article>
+            <img src="/dashboard/users.svg" alt="" />
+            <div>
+              <h2>Gestionar usuarios</h2>
+              <span>Haz click aqui</span>
+            </div>
+          </article>
+
+          <article>
+            <img src="/dashboard/users.svg" alt="" />
+            <div>
+              <h2>Gestionar usuarios</h2>
+              <span>Haz click aqui</span>
+            </div>
+          </article>
+        </main>
+      </section>
     </>
   );
 }
