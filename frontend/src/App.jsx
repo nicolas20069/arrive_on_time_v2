56041b743f0bea598e99ea7b570ec7838b439c5d
juanjk24 <juanjkuellar24@gmail.com>
2024-11-25 23:53:43 -0500

    Add route login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primeicons/primeicons.css";
 
+import { Login } from "./login/index.jsx";
 import { Employees } from "./admin/modules/employees/index.jsx";
 import { Dashboard } from "./admin/modules/dashboard/index.jsx";
 import { Reports } from "./admin/modules/reports/index.jsx";
@@ -14,7 +15,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Iniciar Sesion</div>} />
+        <Route path="/" element={<Login/>} />
         <Route path="/admin" element={<Dashboard/>} />
         <Route path="/admin/employees" element={<Employees/>} />
         <Route path="/admin/reports" element={<Reports/>} />
