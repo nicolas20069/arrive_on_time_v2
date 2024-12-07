f20e9b88e9b0e06ef6180544a9e802671287ebf9
juanjk24 <juanjkuellar24@gmail.com>
2024-12-07 01:41:15 -0500

    Add login route

---
+++ backend/routes/login.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+import { LoginController } from "../controllers/login.js";
+
+export const loginRouter = Router();
+
+loginRouter.post("/", LoginController.login);
+
