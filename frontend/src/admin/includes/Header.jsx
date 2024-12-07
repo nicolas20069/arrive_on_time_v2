22906a3d2864f5dd8d0cfba7cb063be255b089ac
juanjk24 <juanjkuellar24@gmail.com>
2024-12-07 12:34:31 -0500

    Add yourAttendancesAdmin module

--- frontend/src/admin/includes/Header.jsx
+++ frontend/src/admin/includes/Header.jsx
@@ -23,6 +23,7 @@ export function Header() {
     { label: "Usuarios", path: "/admin/users" },
     { label: "Roles", path: "/admin/roles" },
     { label: "Asistencias", path: "/admin/attendances" },
+    { label: "Tus Asistencias", path: "/admin/your-attendances" },
     { label: "Empresas o Instituciones", path: "/admin/companies" },
     /* { label: "Reportes", path: "/admin/reports" }, */
     { label: "Tipos de Asistencias", path: "/admin/attendances-type" },
