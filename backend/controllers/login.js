edf7ef385d9fa94c39dd72114852fc79c0802d4f
juanjk24 <juanjkuellar24@gmail.com>
2024-12-07 01:41:21 -0500

    Add login controller

---
+++ backend/controllers/login.js
@@ -0,0 +1,52 @@
+import jwt from "jsonwebtoken";
+import { UserModel } from "../models/user.js";
+import { SECRET_JWT_KEY } from "../config/global.js";
+
+export class LoginController {
+  static async login(req, res) {
+    const { cedula, contraseña } = req.body;
+
+    if (!cedula || !contraseña) {
+      return res
+        .status(400)
+        .json({ message: "Cedula y contraseña son requeridos" });
+    }
+
+    try {
+      const user = await UserModel.login({ cedula, contraseña });
+      const {
+        user_id,
+        nombres,
+        apellidos,
+        edad,
+        correo,
+        direccion,
+        telefono,
+        empresa_id,
+        rol_id,
+      } = user;
+
+      const token = jwt.sign(
+        {
+          user_id,
+          nombres,
+          apellidos,
+          edad,
+          cedula: user.cedula,
+          correo,
+          direccion,
+          telefono,
+          empresa_id,
+          rol_id,
+        },
+        SECRET_JWT_KEY,
+        { expiresIn: "1h" }
+      );
+
+      res.status(200).json({ message: "Sesion iniciada", user, token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error del servidor" });
+    }
+  }
+}
