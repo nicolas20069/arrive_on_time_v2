df68035e850bdc428a78a911022bddf64ef4e861
juanjk24 <juanjkuellar24@gmail.com>
2025-02-26 10:22:40 -0500

    Add route for resources

--- backend/index.js
+++ backend/index.js
@@ -8,19 +8,25 @@ import { companiesRouter } from "./routes/companies.js";
 import { attendanceRouter } from "./routes/attendance.js";
 import { attendancesTypeRouter } from "./routes/attendancesType.js";
 import { loginRouter } from "./routes/login.js";
+import { resourcesRouter } from "./routes/resources.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors(FRONTEND_URL));
 app.disable("x-powered-by");
 
+// Ruta para gestionar el login
 app.use('/login', loginRouter)
 
+// Rutas para gestionar los modulos 
 app.use('/users', usersRouter)
 app.use('/roles', rolesRouter)
 app.use('/companies', companiesRouter)
 app.use('/attendances', attendanceRouter)
 app.use('/attendances-type', attendancesTypeRouter)
+
+// Rutas para gestionar la descarga de recursos
+app.use('/resources', resourcesRouter)
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
