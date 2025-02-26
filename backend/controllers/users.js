784f524df9c321f3a34e4c3c71725cdbec114293
juanjk24 <juanjkuellar24@gmail.com>
2025-02-26 15:57:08 -0500

    Add controllers for upload images

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,9 @@
 import { UserModel } from "../models/user.js";
 import { validateUser, validateUserUpdate } from "../schemas/users.js";
+import { v2 as cloudinary } from "cloudinary";
+import { cloudinaryConfig } from "../config/cloudinary.js";
 
+cloudinary.config(cloudinaryConfig);
 export class UserController {
   // Método para obtener todos los usuarios
   static async getAll(req, res) {
@@ -8,9 +11,9 @@ export class UserController {
       const users = await UserModel.getAll();
 
       const publicUsers = users.map((user) => {
-        const { contraseña: _, ...publicUser } = user
-        return publicUser
-      })
+        const { contraseña: _, ...publicUser } = user;
+        return publicUser;
+      });
 
       res.json(publicUsers);
     } catch (error) {
@@ -25,14 +28,16 @@ export class UserController {
       const users = await UserModel.getAdmins();
 
       const publicUsers = users.map((user) => {
-        const { contraseña: _, ...publicUser } = user
-        return publicUser
-      })
+        const { contraseña: _, ...publicUser } = user;
+        return publicUser;
+      });
 
       res.json(publicUsers);
     } catch (error) {
       console.log(error);
-      res.status(500).json({ message: "Error al obtener los usuarios administradores" });
+      res
+        .status(500)
+        .json({ message: "Error al obtener los usuarios administradores" });
     }
   }
 
@@ -73,6 +78,64 @@ export class UserController {
     }
   }
 
+  // Método para establecer la imagen de perfil de un usuario
+  static async setImageProfile(req, res) {
+    const { id } = req.params;
+
+    if (!req.file) {
+      return res.status(400).json({ message: "Imagen no encontrada" });
+    }
+
+    const { buffer } = req.file;
+
+    // Subir la imagen a cloudinary
+    const response = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream({
+            folder: "arrive_on_time",
+          },
+          (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+          }
+        )
+        .end(buffer);
+    });
+
+    try {
+      const result = await UserModel.setImageProfile({
+        id,
+        imageUrl: response.url,
+      });
+      res.json({ message: "Imagen subida con éxito", affectedRows: result });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Error al subir la imagen" });
+    }
+  }
+
+  // Metodo para guardar la imagen de perfil en la base de datos
+  static async setImageProfileDB(req, res) {
+    const { id } = req.params;
+
+    if (!req.file) {
+      return res.status(400).json({ message: "Imagen no encontrada" });
+    }
+
+    const { buffer } = req.file;
+
+    try {
+      const result = await UserModel.setImageProfileDB({
+        id,
+        imageData: buffer,
+      });
+      res.json({ message: "Imagen subida con éxito", affectedRows: result });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Error al subir la imagen" });
+    }
+  }
+
   // Método para actualizar un usuario siendo administrador
   static async update(req, res) {
     const { id } = req.params;
@@ -91,7 +154,10 @@ export class UserController {
 
     try {
       const resultUser = await UserModel.update({ id, input: result.data });
-      res.status(201).json({ message: 'Usuario actualizado correctamente', affectedRows: resultUser });
+      res.status(201).json({
+        message: "Usuario actualizado correctamente",
+        affectedRows: resultUser,
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: error.message });
@@ -104,7 +170,10 @@ export class UserController {
 
     try {
       const result = await UserModel.delete({ id });
-      res.json({ message: 'Usuario eliminado correctamente', affectedRows: result });
+      res.json({
+        message: "Usuario eliminado correctamente",
+        affectedRows: result,
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Error al eliminar el usuario" });
