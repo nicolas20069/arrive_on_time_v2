c74ee11f0528d5f77a24ab63790f23048a9dbfda
juanjk24 <juanjkuellar24@gmail.com>
2025-02-26 10:23:07 -0500

    Update database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,11 @@ import mysql from 'mysql2/promise';
 
 import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } from './global.js';
 
-export const pool = mysql.createPool({
+export const dbConfig = {
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    database: DB_DATABASE
-});
+    database: DB_DATABASE,
+}
+
+export const pool = mysql.createPool(dbConfig);
