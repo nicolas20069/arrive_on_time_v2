08d158324396aa2d0e2a474bea3a4a5b3db92e0d
juanjk24 <juanjkuellar24@gmail.com>
2025-02-26 15:57:20 -0500

    Add endpoints for uploads aimges

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { UserController } from "../controllers/users.js";
 
 export const usersRouter = Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 usersRouter.get("/", UserController.getAll);
 usersRouter.get("/admins", UserController.getAdmins);
 usersRouter.get("/:id", UserController.getById);
 
 usersRouter.post("/", UserController.create);
+usersRouter.post("/image-profile/:id", upload.single("user_img_profile"), UserController.setImageProfile);
+usersRouter.post("/image-profile-db/:id", upload.single("user_img_profile"), UserController.setImageProfileDB);
 usersRouter.put("/:id", UserController.update);
 usersRouter.delete("/:id", UserController.delete);
