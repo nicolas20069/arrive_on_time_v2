14b696df80d7e548b2d890d624b95478ce40f4e0
juanjk24 <juanjkuellar24@gmail.com>
2025-03-24 18:04:04 -0500

    Refactor Show Toasts

---
+++ frontend/src/login/constants/toast-config.js
@@ -0,0 +1,11 @@
+export const SEVERITY_TOAST = {
+  success: "success",
+  warn: "warn",
+  error: "error",
+};
+
+export const SUMMARY_TOAST = {
+  success: "Success",
+  warn: "Warning",
+  error: "Error",
+};
