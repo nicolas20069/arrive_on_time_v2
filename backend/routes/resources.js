d5006d51ce90eab91ebf71c410007b6c654fe0d6
juanjk24 <juanjkuellar24@gmail.com>
2025-03-25 15:12:12 -0500

    Añadir middlewares de autenticacion

--- backend/routes/resources.js
+++ backend/routes/resources.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { ResourcesController } from "../controllers/resources.js";
+import { verifyToken, isAdmin} from '../middlewares/auth.js';
 
 export const resourcesRouter = Router();
 
-resourcesRouter.get("/db/:rol_id/:user_id", ResourcesController.getDB);
-resourcesRouter.get("/all-attendances/:rol_id/:user_id", ResourcesController.getAllAttendances);
-resourcesRouter.get("/your-attendances/:id", ResourcesController.getYourAttendances);
+resourcesRouter.get("/db/:rol_id/:user_id", [verifyToken, isAdmin], ResourcesController.getDB);
+resourcesRouter.get("/all-attendances/:rol_id/:user_id", [verifyToken, isAdmin], ResourcesController.getAllAttendances);
+resourcesRouter.get("/your-attendances/:id", verifyToken, ResourcesController.getYourAttendances);
